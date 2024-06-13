528701a6a95d36140d624c2a638daf9680dc144c
Thizz00 <jakubkielb7@gmail.com>
2024-06-13 21:26:17 +0200

    Adding a semaphore task limit for Olx😈.

--- src/olx_scraper.py
+++ src/olx_scraper.py
@@ -82,53 +82,55 @@ async def fetch_links_from_page_sale(page_number,selected_option_city,selected_o
         return set()
     
 
-async def scrap_data_olx_sale(link):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(link, headers=HEADERS) as response:
-                if response.status == 200:
-                    html = await response.text()
-                    soup = BeautifulSoup(html, 'html.parser')
-                    address = [i.text.replace(',', '').replace('Strona główna,Nieruchomości,Mieszkania,Wynajem,Wynajem', '').replace(
-                            'Wynajem', '').replace('Strona główna,Nieruchomości,Mieszkania,, ', '').replace('-', '').replace('Sprzedaż', '').replace(
-                            'Nieruchomości', '').replace('Strona główna', '').replace('Mieszkania', '').replace(',', '').replace(',,,,', '') if i.text != '' else 'No data' for i in soup.find_all(class_=ADRESS_CLASS)]
+async def scrap_data_olx_sale(link,semaphore):
+    async with semaphore: 
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(link, headers=HEADERS) as response:
+                    if response.status == 200:
+                        html = await response.text()
+                        soup = BeautifulSoup(html, 'html.parser')
+                        address = [i.text.replace(',', '').replace('Strona główna,Nieruchomości,Mieszkania,Wynajem,Wynajem', '').replace(
+                                'Wynajem', '').replace('Strona główna,Nieruchomości,Mieszkania,, ', '').replace('-', '').replace('Sprzedaż', '').replace(
+                                'Nieruchomości', '').replace('Strona główna', '').replace('Mieszkania', '').replace(',', '').replace(',,,,', '') if i.text != '' else 'No data' for i in soup.find_all(class_=ADRESS_CLASS)]
 
-                    address = ','.join(address[4:7]).replace('  ','')
+                        address = ','.join(address[4:7]).replace('  ','')
 
-                    price = ",".join([i.text.replace('do negocjacji',' do negocjacji') if i.text != '' else 'No data' for i in soup.find_all(class_=PRICE_CLASS)])
-                    
-                    currrent_date = datetime.now().strftime("%d %m %Y")
+                        price = ",".join([i.text.replace('do negocjacji',' do negocjacji') if i.text != '' else 'No data' for i in soup.find_all(class_=PRICE_CLASS)])
+                        
+                        currrent_date = datetime.now().strftime("%d %m %Y")
 
-                    date = ",".join([currrent_date if 'Dzisiaj' in i.text else i.text.replace('stycznia','01').replace('lutego','02').replace('marca','03').replace('kwietnia','04').replace('maja','05').replace('czerwca','06').replace('lipca','07').replace('sierpnia','08').replace('września','09').replace('października','10').replace('listopada','11').replace('grudnia','12') if i.text != '' else 'No data' for i in soup.find_all(class_=DATE_CLASS)])
+                        date = ",".join([currrent_date if 'Dzisiaj' in i.text else i.text.replace('stycznia','01').replace('lutego','02').replace('marca','03').replace('kwietnia','04').replace('maja','05').replace('czerwca','06').replace('lipca','07').replace('sierpnia','08').replace('września','09').replace('października','10').replace('listopada','11').replace('grudnia','12') if i.text != '' else 'No data' for i in soup.find_all(class_=DATE_CLASS)])
 
-                    data_dict = {'Link': link, 'Data wystawienia': date,'Adres':address , 'Cena': price}
+                        data_dict = {'Link': link, 'Data wystawienia': date,'Adres':address , 'Cena': price}
 
-                    category_text_list = [i.text if i.text != '' else 'No data' for i in soup.find_all(class_=CATEGORY_CLASS_LIST)]
+                        category_text_list = [i.text if i.text != '' else 'No data' for i in soup.find_all(class_=CATEGORY_CLASS_LIST)]
 
-                    keys = ['Cena za m²','Poziom', 'Umeblowane','Rynek','Rodzaj zabudowy', 'Powierzchnia', 'Liczba pokoi']
+                        keys = ['Cena za m²','Poziom', 'Umeblowane','Rynek','Rodzaj zabudowy', 'Powierzchnia', 'Liczba pokoi']
 
 
-                    for key in keys:
-                        pattern = re.compile(f'{key}:(.+?)(?={ "|".join(map(re.escape, keys[1:])) }|$)')
-                        match = pattern.search(category_text_list[0])
-                        data_dict[key] = match.group(1).strip() if match else "No data"
-                    logging.info(f"Data successfully scraped from link: {link}")
-                    return data_dict
-                            
-                else:
-                    logging.warning(f"Failed to fetch {link} - Status code: {response.status}")
+                        for key in keys:
+                            pattern = re.compile(f'{key}:(.+?)(?={ "|".join(map(re.escape, keys[1:])) }|$)')
+                            match = pattern.search(category_text_list[0])
+                            data_dict[key] = match.group(1).strip() if match else "No data"
+                        logging.info(f"Data successfully scraped from link: {link}")
+                        return data_dict
+                                
+                    else:
+                        logging.warning(f"Failed to fetch {link} - Status code: {response.status}")
 
-        except Exception as e:
-            logging.error(f"Error while scraping data from link {link}: {type(e).__name__} - {str(e)}", exc_info=True)
+            except Exception as e:
+                logging.error(f"Error while scraping data from link {link}: {type(e).__name__} - {str(e)}", exc_info=True)
 
 async def main_olx_sale(selected_option_city,selected_option_type,selected_market_type,selected_area):
+    semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
     try:
             num_pages = await get_num_pages_sale(selected_option_city,selected_option_type,selected_market_type,selected_area)
             tasks = [fetch_links_from_page_sale(i,selected_option_city,selected_option_type,selected_market_type,selected_area) for i in range(1, num_pages + 1)]
             results = await asyncio.gather(*tasks)
             scraped_links = set().union(*results)
             logging.info(f"Successfully fetched {len(scraped_links)} URLs")
-            results_scrap_data = await asyncio.gather(*[scrap_data_olx_sale(link) for link in scraped_links])
+            results_scrap_data = await asyncio.gather(*[scrap_data_olx_sale(link,semaphore) for link in scraped_links])
 
             logging.info(f"Scraped: {len(results_scrap_data)} out of: {len(scraped_links)}")
             
