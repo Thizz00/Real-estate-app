2af1122610f6ede18c8920b003d77de2471293c7
Thizz00 <jakubkielb7@gmail.com>
2024-06-15 11:17:42 +0200

    Added the variable OTODOM_LINK_RENT_OPTION_ALL 🦗.

--- config/otodom_config.py
+++ config/otodom_config.py
@@ -1,5 +1,6 @@
 #Links
-OTODOM_LINK_RENT = "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/{selected_option_city}?limit=24&areaMax={area}&daysSinceCreated={number}&by=DEFAULT&direction=DESC&viewType=listing"
+OTODOM_LINK_RENT_OPTION_ALL = "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/{selected_option_city}?limit=24&areaMax={area}&daysSinceCreated={number}&by=DEFAULT&direction=DESC&viewType=listing"
+OTODOM_LINK_RENT = "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/{selected_option_city}?limit=24&areaMax={area}&daysSinceCreated={number}&extras={option_type}&by=DEFAULT&direction=DESC&viewType=listing"
 OTODOM_LINK_SALE = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie{option_market_type}/{selected_option_city}?limit=24&ownerTypeSingleSelect={optiontype}&areaMax={area}&daysSinceCreated={number}&by=DEFAULT&direction=DESC&viewType=listing"
 URL_JOIN = 'https://www.otodom.pl'
 
